18467338aa849fd3801798498130d5fe032bd2bc
lsiten <1304906404@qq.com>
2024-05-12 02:53:14 +0800

    feat: bash

--- ry.sh
+++ ry.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # author ruoyi
 # ./ry.sh start 启动
 # ./ry.sh stop 停止
@@ -8,47 +8,42 @@ AppName=ruoyi-admin.jar
 
 # JVM参数
 JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms512M -Xmx512M -XX:PermSize=256M -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
-APP_HOME=`pwd`
+APP_HOME=$(pwd)
 LOG_PATH=$APP_HOME/logs/$AppName.log
 
-if [ "$1" = "" ];
-then
+if [ "$1" = "" ]; then
     echo -e "\033[0;31m 未输入操作名 \033[0m  \033[0;34m {start|stop|restart|status} \033[0m"
     exit 1
 fi
 
-if [ "$AppName" = "" ];
-then
+if [ "$AppName" = "" ]; then
     echo -e "\033[0;31m 未输入应用名 \033[0m"
     exit 1
 fi
 
-function start()
-{
-    PID=`ps -ef |grep java|grep $AppName|grep -v grep|awk '{print $2}'`
+start() {
+    PID=$(ps -ef | grep java | grep $AppName | grep -v grep | awk '{print $2}')
 
     if [ x"$PID" != x"" ]; then
         echo "$AppName is running..."
     else
-        nohup java -jar  $JVM_OPTS target/$AppName > /dev/null 2>&1 &
+        nohup java -jar $JVM_OPTS target/$AppName > /dev/null 2>&1 &
         echo "Start $AppName success..."
     fi
 }
 
-function stop()
-{
+stop() {
     echo "Stop $AppName"
     PID=""
-    query(){
-        PID=`ps -ef |grep java|grep $AppName|grep -v grep|awk '{print $2}'`
+    query() {
+        PID=$(ps -ef | grep java | grep $AppName | grep -v grep | awk '{print $2}')
     }
 
     query
     if [ x"$PID" != x"" ]; then
         kill -TERM $PID
         echo "$AppName (pid:$PID) exiting..."
-        while [ x"$PID" != x"" ]
-        do
+        while [ x"$PID" != x"" ]; do
             sleep 1
             query
         done
@@ -58,17 +53,15 @@ function stop()
     fi
 }
 
-function restart()
-{
+restart() {
     stop
     sleep 2
     start
 }
 
-function status()
-{
-    PID=`ps -ef |grep java|grep $AppName|grep -v grep|wc -l`
-    if [ $PID != 0 ];then
+status() {
+    PID=$(ps -ef | grep java | grep $AppName | grep -v grep | wc -l)
+    if [ $PID != 0 ]; then
         echo "$AppName is running..."
     else
         echo "$AppName is not running..."
@@ -76,16 +69,21 @@ function status()
 }
 
 case $1 in
-    start)
-    start;;
-    stop)
-    stop;;
-    restart)
-    restart;;
-    status)
-    status;;
-    *)
+start)
+    start
+    ;;
+stop)
+    stop
+    ;;
+restart)
+    restart
+    ;;
+status)
+    status
+    ;;
+*)
     echo -e "\033[0;31m Invalid command: $1 \033[0m"
     echo -e "\033[0;34m Usage: $0 {start|stop|restart|status} \033[0m"
     exit 1
+    ;;
 esac
